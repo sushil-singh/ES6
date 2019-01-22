aa4f622d011ffcfd8cece4cfd7d15d6f2bac6ce7
Sushil Kumar <sushil.java9@gmail.com>
2019-01-22 10:51:50 +0200

    Add files via upload

---
+++ destructuring_assignments.js
@@ -0,0 +1,7 @@
+var foo = ['one', 'two', 'three'];
+
+var [abc, wxy, efg] = foo;
+
+console.log(abc); // "one"
+console.log(wxy); // "two"
+console.log(efg); // "three"
---
+++ js_map.js
@@ -0,0 +1,11 @@
+let map = new Map();
+
+map.set('foo', 123);
+console.log(map.get('foo'))
+
+
+map.has('foo')
+
+map.delete('foo')
+
+map.has('foo')
